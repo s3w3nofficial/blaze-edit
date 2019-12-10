e4015bfb84b9b2e0bab47abe0b1ea48897db9868
majda107 <majda.trpkos@gmail.com>
2019-12-10 07:57:34 +0000

    Deploying to gh-pages from master ae741798441648a36bb6933a5eb444d7e384253d

---
+++ _content/BlazeEdit/blazeedit.js
@@ -0,0 +1,14 @@
+﻿let canvas;
+
+function loadCanvas() {
+    canvas = document.querySelector(`.blazeedit-canvas`);
+}
+
+function getHeightFromRef(element) {
+    var el = document.querySelector(`.${element}`);
+    return el.clientHeight;
+}
+
+function getCanvasTopOffset() {
+    return parseInt(canvas.getBoundingClientRect().top);
+}
